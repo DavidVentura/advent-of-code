1de413b78727b7dedd0c61c7dd0f9f95f35d3d61
David Ventura <david@davidv.dev>
2025-12-06 14:28:26 +0100

    store func pointer instead of char

--- 2025/6/main.zig
+++ 2025/6/main.zig
@@ -13,6 +13,14 @@ pub fn main() !void {
     std.debug.print("res {}\n", .{res});
 }
 
+const OpFn = *const fn (u64, u64) u64;
+fn add(a: u64, b: u64) u64 {
+    return a + b;
+}
+fn mul(a: u64, b: u64) u64 {
+    return a * b;
+}
+
 pub fn process_file(alloc: std.mem.Allocator, fname: []const u8) !u64 {
     const file = try std.fs.cwd().openFile(fname, .{});
     defer file.close();
@@ -32,11 +40,15 @@ pub fn process_file(alloc: std.mem.Allocator, fname: []const u8) !u64 {
         try lines.append(alloc, line);
     }
 
-    var ops: std.ArrayList(u8) = .empty;
+    var ops: std.ArrayList(OpFn) = .empty;
     var columns_start: std.ArrayList(u64) = .empty;
     for (last_line, 0..) |c, i| {
-        if (c == '+' or c == '*') {
-            try ops.append(alloc, c);
+        if (c == '+') {
+            try ops.append(alloc, add);
+            try columns_start.append(alloc, i);
+        }
+        if (c == '*') {
+            try ops.append(alloc, mul);
             try columns_start.append(alloc, i);
         }
     }
@@ -56,7 +68,7 @@ pub fn process_file(alloc: std.mem.Allocator, fname: []const u8) !u64 {
         const max_digits: u64 = col_end - col_start;
 
         const op = ops.items[column];
-        var col_total: u64 = if (op == '+') 0 else 1; // identity
+        var col_total: u64 = if (op == add) 0 else 1; // identity
         for (0..max_digits) |di| {
             var vertical_num: u64 = 0;
             for (0..rows) |row| {
@@ -71,12 +83,8 @@ pub fn process_file(alloc: std.mem.Allocator, fname: []const u8) !u64 {
                 const digit = try std.fmt.charToDigit(maybe_digit, 10);
                 vertical_num = vertical_num * 10 + digit;
             }
-            println("vertical_num {}, op {c}", .{ vertical_num, op });
-            if (op == '+') {
-                col_total += vertical_num;
-            } else {
-                col_total *= vertical_num;
-            }
+            println("vertical_num {}", .{vertical_num});
+            col_total = op(col_total, vertical_num);
         }
         println("col_total {}", .{col_total});
         res += col_total;
